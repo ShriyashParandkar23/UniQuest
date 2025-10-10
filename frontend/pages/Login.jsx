a6b48fdafa37967110c5200a81303ecd4800ef64
Anurag Chakraborty <anuragchakraborty128@gmail.com>
2025-10-10 17:49:15 +0100

    Added Login Functionalities
    If user is logged in User name displayed and menu displayed, else login sign up and menu disable

--- frontend/pages/Login.jsx
+++ frontend/pages/Login.jsx
@@ -1,15 +1,28 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from '@clerk/clerk-react';
+import React, { useEffect } from 'react'
+import { SignIn, useUser } from '@clerk/clerk-react'
+import { useNavigate } from 'react-router-dom'
+import Header from '../components/Header'
+import Dashboard from './Dashboard'
 
 export default function Login() {
+    const { isLoaded, isSignedIn, user } = useUser()
+    const navigate = useNavigate()
+    //console.log(isLoaded, isSignedIn, user.firstName)
+
+    useEffect(() => {
+    if (isLoaded && isSignedIn) {
+        console.log(user.firstName)
+      navigate('/dashboard', { replace: true })
+    }
+  }, [isLoaded, isSignedIn, navigate])
     return (
-    <header>
-        <SignedOut>
-            <SignInButton />
-        </SignedOut>
-            
-        <SignedIn>
-            <UserButton />
-        </SignedIn>
-    </header>
+        <>
+        <Header />
+        <div className="min-h-screen flex items-center justify-center bg-gray-50">
+            <div className="w-full max-w-md p-6">
+            <SignIn path="/login" routing="path" />
+        </div>
+      </div>
+    </>
     )
 }
