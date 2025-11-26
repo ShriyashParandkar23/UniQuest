ffeb44bcd8e5db7ad4a581a0171d49a19d29b94f
SSPAWAR1 <120547796+SSPAWAR1@users.noreply.github.com>
2025-11-26 10:01:42 +0000

    Feature/backend 11nov (#7)
    * Adding backend
    
    * made changes to backend
    
    ---------
    
    Co-authored-by: Shaunak <shaunak@Shaunaks-MacBook-Air.local>

---
+++ UniQuest_Backend/accounts/views.py
@@ -0,0 +1,41 @@
+from rest_framework import generics, status
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+from rest_framework_simplejwt.tokens import RefreshToken
+from django.contrib.auth import authenticate
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework.permissions import AllowAny
+
+
+
+
+User = get_user_model()
+
+def get_tokens_for_user(user):
+    refresh = RefreshToken.for_user(user)
+    return {
+        'refresh': str(refresh),
+        'access': str(refresh.access_token),
+    }
+
+class RegisterView(generics.CreateAPIView):
+    queryset = User.objects.all()
+    permission_classes = [AllowAny]
+    serializer_class = RegisterSerializer
+
+class LoginView(generics.GenericAPIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request):
+        email = request.data.get("email")
+        password = request.data.get("password")
+        user = authenticate(email=email, password=password)
+        if user is not None:
+            tokens = get_tokens_for_user(user)
+            return Response({
+                "user": {"id": user.id, "email": user.email, "username": user.username},
+                "tokens": tokens
+            })
+        return Response({"error": "Invalid credentials"}, status=status.HTTP_401_UNAUTHORIZED)
+
